cd2f0dab01ae63d015af7715334cc99373cdac2d
fuchsia-pragmaflow <104771594+fuchsia-pragmaflow@users.noreply.github.com>
2022-05-24 10:02:07 -0400

    My commit message

--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen } from './LoginScreen'
-import { HomeScreen } from './HomeScreen'
-import { RegisterScreen } from './RegisterScreen'
+import { Screen } from './Screen'
 type RootStackNavigator = {
-   LoginScreen: undefined,
-   HomeScreen: undefined,
-   RegisterScreen: undefined,
+   Screen: undefined,
 }
 const Stack = createStackNavigator<RootStackNavigator>();
 export function RootNavigator() {
    return (
-      <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{
+      <Stack.Navigator initialRouteName="Screen" screenOptions={{
         headerShown: false,
         gestureEnabled: true,
         animationEnabled: false,
       }}>
-         <Stack.Screen name="LoginScreen" component={LoginScreen} />
-         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+         <Stack.Screen name="Screen" component={Screen} />
       </Stack.Navigator>
    )
 }
---
+++ src/Screen.tsx
@@ -0,0 +1,21 @@
+import { Screen, Container, Text, TextInput, Button } from '@fuchsia-for-all/primitives'
+import React, { useState } from 'react'
+
+export function Screen () {
+   const [UsernameTextInputtext, setUsernameTextInputtext] = useState<string>()
+   const [PasswordTextInputtext, setPasswordTextInputtext] = useState<string>()
+
+   return (
+      <Screen>
+         <Container style={{"height":150,"borderRadius":5,"backgroundColor":"#f8e71c","display":"flex","position":"initial","margin":{"left":"10","right":"10","top":"10","bottom":"10"},"padding":{"left":"0"}}} >
+            <Text properties={{"text":"Lorem Ipsum"}} style={{"height":24,"width":160,"color":"#000000","display":"inline","position":"initial"}} />
+            <TextInput text={{ value: UsernameTextInputtext, onChange: setUsernameTextInputtext}} properties={{"placeholder":"Enter...","text":"undefined"}} style={{"height":24,"width":160,"color":"#000","borderColor":"#000","borderStyle":"solid","borderWidth":1,"borderRadius":5}} />
+            <Text properties={{"text":"Lorem Ipsum"}} style={{"height":24,"width":160,"color":"#000000","display":"inline","position":"initial"}} />
+            <TextInput text={{ value: PasswordTextInputtext, onChange: setPasswordTextInputtext}} properties={{"placeholder":"Enter...","text":"undefined"}} style={{"height":24,"width":160,"color":"#000","borderColor":"#000","borderStyle":"solid","borderWidth":1,"borderRadius":5}} />
+            <Button properties={{"title":"Lorem Ipsum"}} style={{"height":35,"width":175,"backgroundColor":"#417505","borderRadius":5,"display":"flex","position":"initial","placement":"initial"}} >
+               <Text properties={{"text":"Lorem Ipsum"}} style={{"height":24,"width":160,"color":"#ffffff","display":"inline","position":"initial"}} />
+            </Button>
+         </Container>
+      </Screen>
+   )
+}
